9e42797a55cf2e0da099e3e4a81eb53b386e02a8
TianFeng081 <kelvin.lau1234@gmail.com>
2026-08-31 19:18:21 +0800

    build: add public Morphe patch metadata

---
+++ patches/build.gradle.kts
@@ -0,0 +1,13 @@
+group = "dev.ytmcar"
+
+patches {
+    about {
+        name = "YTM CAR Patches"
+        description = "YouTube Music CAR patches for Android Auto app-side compatibility"
+        source = "https://github.com/TianFeng081/ytm-car-patches-release"
+        author = "TianFeng081"
+        contact = "https://github.com/TianFeng081"
+        website = "https://github.com/TianFeng081/ytm-car-patches-release"
+        license = "GPL-3.0"
+    }
+}
